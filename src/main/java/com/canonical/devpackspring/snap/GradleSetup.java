2b8140de91dba173f6e869067bf215ab20109986
Vladimir Petko <vladimir.petko@canonical.com>
2025-02-24 13:40:09 +1300

    feat: implement setup gradle and maven commands
    - move command file back to commands (annotation processor picks only stuff there)
    - move gradle/maven setup from configure project.

---
+++ src/main/java/com/canonical/devpackspring/snap/GradleSetup.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.canonical.devpackspring.snap;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+public interface GradleSetup {
+
+	static String setupGradle(Snap snap) throws IOException {
+		File gradleInitDir = new File(String.valueOf(Paths.get(System.getProperty("user.home"), ".gradle", "init.d")));
+		GradleInit gradleInit = new GradleInit(gradleInitDir);
+		if (!gradleInit.addGradletInitFile(snap)) {
+			return "Spring boot init file '" + snap.name() + ".gradle' was already added to " + gradleInitDir;
+		}
+		return "Spring boot init file '" + snap.name() + ".gradle' was added to " + gradleInitDir;
+	}
+
+}
---
+++ src/main/java/com/canonical/devpackspring/snap/MavenSetup.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.canonical.devpackspring.snap;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.xpath.XPathExpressionException;
+
+import org.xml.sax.SAXException;
+
+public interface MavenSetup {
+
+	static String setupMaven(Snap snap) throws ParserConfigurationException, SAXException, IOException,
+			XPathExpressionException, TransformerException {
+		File m2settings = new File(String.valueOf(Paths.get(System.getProperty("user.home"), ".m2")));
+		Settings settings = new Settings(m2settings);
+		if (!settings.addMavenProfile(snap)) {
+			return "Spring boot profile '" + snap.name() + "' is already present in maven user settings file "
+					+ m2settings;
+		}
+		File settingsFile = new File(m2settings, "settings.xml");
+		try (BufferedWriter wr = new BufferedWriter(new FileWriter(settingsFile))) {
+			wr.write(settings.toXml());
+		}
+		return "Spring boot profile '" + snap.name() + "' was added to maven user settings file " + m2settings;
+	}
+
+}
